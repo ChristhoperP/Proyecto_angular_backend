8b02e8ed25bca272594e7503440af723e7966527
ChristhoperP <43559198+ChristhoperP@users.noreply.github.com>
2020-09-17 15:58:51 -0600

    probando con conexion por socket

--- conexion.js
+++ conexion.js
@@ -4,7 +4,8 @@ const { Pool } = require('pg');
 
 module.exports = {
     pool : new Pool({
-        host: '34.69.154.201',
+        //host: '34.69.154.201',
+        host: '/cloudsql/primeval-legacy-289419:us-central1:cactusbbdd',
         user: 'postgres',
         password: 'postgres',
         database: 'postgres',
