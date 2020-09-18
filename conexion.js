e73ca45fa22bc7bf6f738cd127741a95456dcc48
ChristhoperP <43559198+ChristhoperP@users.noreply.github.com>
2020-09-17 19:06:23 -0600

    unix_socket

--- conexion.js
+++ conexion.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const { Pool } = require('pg');
+var dbSocketPath = '/cloudsql/primeval-legacy-289419:us-central1:cactusbbdd'
 
 module.exports = {
     pool : new Pool({
         //host: '34.69.154.201',
-        host: '/cloudsql/primeval-legacy-289419:us-central1:cactusbbdd',
+        unix_socket: `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`,
         user: 'postgres',
         password: 'postgres',
         database: 'postgres',
